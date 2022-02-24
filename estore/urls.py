ab13b2c30dbb63fb054483472b817a046b300c73
1Nelsonel <nelsonmokoro37@gmail.com>
2022-02-24 19:06:35 +0300

    success

--- estore/urls.py
+++ estore/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # app homepage
     path('shop', views.shop, name='shop'),
-    path('property_single', views.property_single, name='property_single'), 
+    path('product', views.product, name='product'), 
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
     path('blog', views.blog, name='blog'), 
-    path('blog_single', views.blog_single, name='blog-single'), 
+    path('blog_single', views.blog_single, name='blog_single'),
+    path('agents', views.agents, name='agents'), 
     path('agent_single', views.agent_single, name='agent_single'),  
     ]
--- estore/views.py
+++ estore/views.py
@@ -5,16 +5,18 @@ from django.shortcuts import render
 def index(request):
         return render(request, 'index.html')
 def shop(request):
-        return render(request, 'property-grid.html')
-def property_single(request):
-        return render(request, 'property-single.html')
+        return render(request, 'shop.html')
+def product(request):
+        return render(request, 'product.html')
 def contact(request):
         return render(request, 'contact.html')   
 def about(request):
         return render(request, 'about.html')
 def blog(request):
-        return render(request, 'blog-grid.html')
+        return render(request, 'blog.html')
 def blog_single(request):
         return render(request, 'blog-single.html')
+def agents(request):
+        return render(request, 'agents.html')
 def agent_single(request):
         return render(request, 'agent-single.html')
