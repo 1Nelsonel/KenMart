38f303ec95d389c16b57ee174b5a95da6ae41309
1Nelsonel <nelsonmokoro37@gmail.com>
2022-04-19 17:54:35 +0300

    success

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:', user)
+        if(user === 'AnonymousUser'){
+            console.log('Not LoggedIn')
+        }else{
+            console.log('User is loggedIn, sending data..')
+        }
+    })
+}
